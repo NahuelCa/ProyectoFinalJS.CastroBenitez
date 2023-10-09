a4ca43b3f95dd7ac1a64f1edb5ee25fd81cfb2ea
NahuelCa <castronahuelunlu@gmail.com>
2023-10-09 20:51:10 -0300

    V3 Calc.IMC&TMB

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-//Paneles y botones principales.
+// Paneles y botones principales.
 const generadorButton = document.getElementById('generador');
 const calculadoraButton = document.getElementById('calculadora');
 const container = document.getElementById('container');
+const resultadosDiv = document.getElementById('resultados');
+const historialTable = document.getElementById('historialTable');
+const tbody = historialTable.querySelector('tbody');
+const guardarBoton = document.getElementById('guardarHistorial');
+const mostrarHistorialBtn = document.getElementById("mostrarHistorial");
+const historialContainer = document.querySelector(".table-container");
+const generarBoton = document.getElementById('generarBoton');
+const volverButton = document.getElementById('volver');
 
+// Eventos para trasladar paneles.
 generadorButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+    container.classList.add("right-panel-active");
 });
 
 calculadoraButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+    container.classList.remove("right-panel-active");
 });
 
 // Función para calcular e interpretar el IMC.
@@ -16,8 +25,8 @@ function calcularIMC(peso, altura) {
     let imc = peso / (altura * altura);
     return imc;
 }
-function interpretarIMC(imc) {
 
+function interpretarIMC(imc) {
     if (imc < 18) {
         return "Bajo peso (insuficiencia ponderal)";
     } else if (imc < 25) {
@@ -25,7 +34,7 @@ function interpretarIMC(imc) {
     } else if (imc < 30) {
         return "Sobrepeso leve";
     } else if (imc < 35) {
-        return "sobrepeso moderado";
+        return "Sobrepeso moderado";
     } else if (imc < 40) {
         return "Obesidad";
     } else {
@@ -41,9 +50,9 @@ function calcularTMB(sexo, peso, altura, edad, nivelActividad) {
     } else if (sexo === 'femenino') {
         tmb = 447.593 + (9.247 * peso) + (3.098 * altura * 100) - (4.330 * edad);
     } else {
-        return "Sexo no válido";
+        throw new Error("Sexo no válido");
     }
-    
+
     if (nivelActividad === 'ligero') {
         tmb *= 1.375;
     } else if (nivelActividad === 'moderado') {
@@ -54,30 +63,29 @@ function calcularTMB(sexo, peso, altura, edad, nivelActividad) {
 
     return tmb;
 }
+
 function interpretarTMB(tmb) {
-	let tmbEntero = Math.round(tmb);
-    let mensaje = "Tu TMB es: " + tmbEntero + " kcal por día.\n";  
+    let tmbEntero = Math.round(tmb);
+    let mensaje = "Tu TMB es: " + tmbEntero + " kcal por día.\n";
     return mensaje;
 }
-	
-// Funcion principal de la aplicacion web.
+
+// Función principal de la aplicación web.
 function calcularIMCyTMB() {
-	
-	// Obténer los valores ingresados por el usuario desde los campos del formulario.
+    // Obtener los valores ingresados por el usuario desde los campos del formulario.
     let peso = parseFloat(document.getElementById("peso").value);
     let altura = parseFloat(document.getElementById("estatura").value);
     let sexo = document.querySelector('input[name="sexo"]:checked').value;
     let edad = parseInt(document.getElementById("edad").value);
     let nivelActividad = document.querySelector('input[name="actividad"]:checked').value;
 
-// Llamar a las funciones para calcular IMC y TMB.
+    // Llamar a las funciones para calcular IMC y TMB.
     let imc = calcularIMC(peso, altura);
     let interpretacionIMC = interpretarIMC(imc);
     let tmb = calcularTMB(sexo, peso, altura, edad, nivelActividad);
     let interpretacionTMB = interpretarTMB(tmb);
 
-// Mostrar los resultados en el div de resultados.
-    let resultadosDiv = document.getElementById("resultados");
+    // Mostrar los resultados en el div de resultados.
     resultadosDiv.innerHTML = `<p>Tu IMC es: ${imc.toFixed(2)} ${interpretacionIMC}</p>`;
     resultadosDiv.innerHTML += `<p>${interpretacionTMB}</p>`;
 }
@@ -105,15 +113,14 @@ function generarDatosAleatorios() {
 
 // Función para mostrar un ejemplo de cómo funciona la calculadora.
 function aprenderCalculadora() {
-
     const ejemploPersona = generarDatosAleatorios();
 
     const imcEjemplo = calcularIMC(ejemploPersona.peso, ejemploPersona.altura);
     const interpretacionIMCEjemplo = interpretarIMC(imcEjemplo);
     const tmbEjemplo = calcularTMB(ejemploPersona.genero, ejemploPersona.peso, ejemploPersona.altura, ejemploPersona.edad, ejemploPersona.nivelActividad);
     const interpretacionTMBEjemplo = interpretarTMB(tmbEjemplo);
-	
-  // Actualizar los textos con los datos generados y mostrar los resultados.
+
+    // Actualizar los textos con los datos generados y mostrar los resultados.
     const pesoLabel = document.getElementById('pesoej');
     const alturaLabel = document.getElementById('estaturaej');
     const edadLabel = document.getElementById('edadej');
@@ -134,115 +141,92 @@ function aprenderCalculadora() {
     `;
 }
 
-  // Referencia y evento en botón "Generar".
-    const generarBoton = document.getElementById('generarBoton');
-    generarBoton.addEventListener('click', function () {
-
-// Llama a la función aprenderCalculadora()
+// Evento para el botón "Generar".
+generarBoton.addEventListener('click', function () {
     aprenderCalculadora();
 });
-    
-// Logica y funcionamiento del historial.
-const historialTable = document.getElementById('historialTable');
-const tbody = historialTable.querySelector('tbody');
 
-const guardarBoton = document.getElementById('guardarHistorial');
-
-guardarBoton.addEventListener('click', () => {
-const mostrarHistorialBtn = document.getElementById("mostrarHistorial");
-const historialContainer = document.querySelector(".table-container");
-  mostrarHistorialBtn.addEventListener("click", function() {
-  historialContainer.classList.toggle("active");
+// Evento para mostrar el historial.
+mostrarHistorialBtn.addEventListener("click", function () {
+    historialContainer.classList.toggle("active");
 });
 
-  // Obtener los datos y resultados del usuario.
-  const peso = parseFloat(document.getElementById("peso").value);
-  const altura = parseFloat(document.getElementById("estatura").value);
-  const sexo = document.querySelector('input[name="sexo"]:checked').value;
-  const edad = parseInt(document.getElementById("edad").value);
-  const nivelActividad = document.querySelector('input[name="actividad"]:checked').value;
-  const imc = calcularIMC(peso, altura);
-  const interpretacionIMC = interpretarIMC(imc);
-  const tmb = calcularTMB(sexo, peso, altura, edad, nivelActividad);
-  const interpretacionTMB = interpretarTMB(tmb);
+// Evento para el botón "Guardar".
+guardarBoton.addEventListener('click', () => {
+    // Obtener los datos y resultados del usuario.
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("estatura").value);
+    const sexo = document.querySelector('input[name="sexo"]:checked').value;
+    const edad = parseInt(document.getElementById("edad").value);
+    const nivelActividad = document.querySelector('input[name="actividad"]:checked').value;
+    const imc = calcularIMC(peso, altura);
+    const interpretacionIMC = interpretarIMC(imc);
+    const tmb = calcularTMB(sexo, peso, altura, edad, nivelActividad);
+    const interpretacionTMB = interpretarTMB(tmb);
 
-  // Crear un objeto JSON con los datos del usuario.
-  const usuarioDatos = {
-    peso: peso,
-    altura: altura,
-    sexo: sexo,
-    edad: edad,
-    nivelActividad: nivelActividad,
-    imc: imc,
-    interpretacionIMC: interpretarIMC(imc),
-    tmb: tmb,
-    interpretacionTMB: interpretarTMB(tmb)
-  };
+    // Crear un objeto JSON con los datos del usuario.
+    const usuarioDatos = {
+        peso: peso,
+        altura: altura,
+        sexo: sexo,
+        edad: edad,
+        nivelActividad: nivelActividad,
+        imc: imc,
+        interpretacionIMC: interpretarIMC(imc),
+        tmb: tmb,
+        interpretacionTMB: interpretarTMB(tmb)
+    };
 
-  // Convertir el objeto JSON a una cadena JSON.
-  const datosJSON = JSON.stringify(usuarioDatos);
+    // Convertir el objeto JSON a una cadena JSON.
+    const datosJSON = JSON.stringify(usuarioDatos);
 
-  // Guardar la cadena JSON en el almacenamiento local con un identificador único.
-  const historial = JSON.parse(localStorage.getItem('historial')) || [];
-  historial.push(usuarioDatos);
-  localStorage.setItem('historial', JSON.stringify(historial));
+    // Guardar la cadena JSON en el almacenamiento local con un identificador único.
+    const historial = JSON.parse(localStorage.getItem('historial')) || [];
+    historial.push(usuarioDatos);
+    localStorage.setItem('historial', JSON.stringify(historial));
 
-  // Actualizar la tabla del historial con los datos guardados.
-  actualizarTablaHistorial();
+    // Actualizar la tabla del historial con los datos guardados.
+    actualizarTablaHistorial();
+});
 
-  // Mostrar una confirmación al usuario.
-  alert('Los resultados se han guardado en el historial.');
+// Evento para borrar una fila del historial (por implementar).
+
+// Evento para vaciar todo el historial.
+const vaciarHistorialBoton = document.getElementById('vaciarHistorial');
+vaciarHistorialBoton.addEventListener('click', () => {
+    localStorage.removeItem('historial');
+    actualizarTablaHistorial();
 });
 
 // Función para llenar la tabla con los datos del historial.
 function actualizarTablaHistorial() {
-  const historial = JSON.parse(localStorage.getItem('historial')) || [];
+    const historial = JSON.parse(localStorage.getItem('historial')) || [];
 
-  // Llenar la tabla con los datos del historial utilizando historialTable y tbody.
-  historial.forEach((usuarioDatos, index) => {
-    const newRow = tbody.insertRow();
-    newRow.insertCell().textContent = new Date().toLocaleDateString(); 
-    newRow.insertCell().textContent = usuarioDatos.peso;
-    newRow.insertCell().textContent = usuarioDatos.altura;
-    newRow.insertCell().textContent = usuarioDatos.sexo;
-    newRow.insertCell().textContent = usuarioDatos.edad;
-    newRow.insertCell().textContent = usuarioDatos.nivelActividad;
-    newRow.insertCell().textContent = usuarioDatos.imc.toFixed(2);
-    newRow.insertCell().textContent = usuarioDatos.tmb.toFixed(2);
-  });
+    // Limpiar la tabla antes de volver a llenarla.
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+    // Llenar la tabla con los datos del historial.
+    historial.forEach((usuarioDatos, index) => {
+        const newRow = tbody.insertRow();
+        newRow.insertCell().textContent = new Date().toLocaleDateString();
+        newRow.insertCell().textContent = usuarioDatos.peso;
+        newRow.insertCell().textContent = usuarioDatos.altura;
+        newRow.insertCell().textContent = usuarioDatos.sexo;
+        newRow.insertCell().textContent = usuarioDatos.edad;
+        newRow.insertCell().textContent = usuarioDatos.nivelActividad;
+        newRow.insertCell().textContent = usuarioDatos.imc.toFixed(2);
+        newRow.insertCell().textContent = usuarioDatos.tmb.toFixed(2);
+    });
 }
+
+// Evento para el botón "Volver" del historial.
+volverButton.addEventListener('click', () => {
+    historialContainer.classList.remove('active');
+});
 
 // Llamar a la función para cargar el historial cuando la página se cargue.
 window.addEventListener('DOMContentLoaded', () => {
-  actualizarTablaHistorial();
-});
-
-// Manejo y modificación de los datos guardados en el historial.
-
-const borrarFilaBoton = document.getElementById('borrarFila');
-const vaciarHistorialBoton = document.getElementById('vaciarHistorial');
-
-// Manejador de evento para borrar una fila. borrarFilaBoton.addEventListener('click', () => {
-// Aca quiero agregar la lógica para borrar la fila deseada de la tabla del historial permitiendo que el usuario seleccione dicha fila individualmente, pero no me dio el tiempo jeje });
-
-// Manejador de evento para vaciar todo el historial.
-vaciarHistorialBoton.addEventListener('click', () => {
-  localStorage.removeItem('historial');
-  actualizarTablaHistorial();
-});
-
-// Función para actualizar la tabla del historial.
-function actualizarTablaHistorial() {
-  const historial = JSON.parse(localStorage.getItem('historial')) || [];
-
-  // Limpiar el contenido actual de la tabla.
-  tbody.innerHTML = '';
-}
-
-  // Evento para el botón "Volver" del historial.
-const volverButton = document.getElementById('volver');
-const historialContainer = document.querySelector('.table-container');
-
-volverButton.addEventListener('click', () => {
-  historialContainer.classList.remove('active');
+    actualizarTablaHistorial();
 });
